f505ec35a46e17bc2aea5ee7f02bf7ad2d1e9ba9
AdryanHolanda <adryanhprofissional@gmail.com>
2023-09-27 14:40:27 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <componentUm />
+</template>
+
+<script>
+import componentUm from './components/componentUm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    componentUm,
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/componentUm.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>Faça sua pergunta: </h1>
+    <input type="text" v-model="pergunta">
+    <button @click="gerarResp">Enviar</button>
+    <h2>  <p>{{perguntaExibida}} </p></h2>
+     <p> {{ resposta }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'componentUm',
+  data() {
+    return {
+      pergunta: "",
+      resposta: "",
+      nRandom: "",
+      respostas: "",
+    }
+  },
+  methods: {
+    gerarResp() {
+      this.perguntaExibida= this.pergunta;
+       this.respostas = [
+        'Com certeza', 
+        'É... não tenho certeza!', 
+        'Claro que não', 
+        'Acho difícil', 
+        'Possivelmente não'
+      ];
+
+      this.nRandom = Math.floor(Math.random() * 5);
+      this.resposta = this.respostas[this.nRandom];
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
